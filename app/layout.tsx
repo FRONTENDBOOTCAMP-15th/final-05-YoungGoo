5bc513f530225935df48ecb06e4f36bffac073f7
seungjunn100 <seungjunn100@gmail.com>
2026-01-26 10:27:44 +0900

    feat: 공통 컴포넌트 UI 구현

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { pretendard } from './fonts';
-import Link from 'next/link';
+import Header from '@/components/common/Header';
+import Footer from '@/components/common/Footer';
 
 export default function RootLayout({
   children,
@@ -9,32 +10,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" className={pretendard.variable}>
-      <body>
-        <ul className="flex gap-4 sticky top-0">
-          <li>
-            <Link href="/">메인 페이지</Link>
-          </li>
-          <li>
-            <Link href="/survey">설문 페이지</Link>
-          </li>
-          <li>
-            <Link href="/products">상품 목록</Link>
-          </li>
-          <li>
-            <Link href="/mypage">마이페이지</Link>
-          </li>
-          <li>
-            <Link href="/subscription">결제(구독)</Link>
-          </li>
-          <li>
-            <Link href="/login">로그인</Link>
-          </li>
-          <li>
-            <Link href="/signup">회원가입</Link>
-          </li>
-        </ul>
+      <body className="min-w-90">
+        <Header />
 
         {children}
+
+        <Footer />
       </body>
     </html>
   );
--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,12 +1,16 @@
-import AuthButton from '@/components/auth/AuthButton';
 import AuthInput from '@/components/auth/AuthInput';
+import BaseButton from '@/components/common/BaseButton';
 
 export default function LoginForm() {
   return (
     <form action="">
       <AuthInput label="이메일" name="email" type="email" placeholder="이메일을 입력하세요." error="이메일 형식이 올바르지 않습니다." />
       <AuthInput label="비밀번호" name="password" type="password" placeholder="비밀번호를 입력하세요." error="비밀번호는 최소 8자 이상이어야 합니다." />
-      <AuthButton type="submit">로그인</AuthButton>
+      <div className="mt-15">
+        <BaseButton type="submit" size="xl" variant="primary">
+          로그인
+        </BaseButton>
+      </div>
       <p className="mt-3 pl-6.5 text-[14px] text-yg-warning">아이디 또는 비밀번호가 잘못되었습니다. 정확히 입력해 주세요.</p>
     </form>
   );
